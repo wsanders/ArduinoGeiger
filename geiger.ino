dc851b99ebdac7b0eea79772956b12141792ed55
W Sanders <wsanders@wsanders.net>
2023-02-13 10:32:08 -0800

    Fix tube quenching deterioration problem

--- geiger.ino
+++ geiger.ino
@@ -1,19 +1,33 @@
-// geiger
-// CPM to uSv/h ratio: 153.8 CPM per uSv/h = 6.502 nSv/h per CPM
-// = 56.996 uSv per year per CPM. 
-// Background dose received by an average person per year, including medical
-//  scans = 4000 uSv. (XKCD)
-// 1 REM = 10000 uSv
+/* geiger
+ CPM to uSv/h ratio: 153.8 CPM per uSv/h = 6.502 nSv/h per CPM
+    = 56.996 uSv per year per CPM.
+ Background dose received by an average person per year, including medical
+    scans = 4000 uSv. (XKCD)
+ 1 REM = 10000 uSv
+
+Changes to account for tube degradation (impending failure?) After a
+couple months' continuous operation the tube because photosensitive,
+then a month later the tube began failing to quench properly, resulting
+in bursts of 2 to 10 pulses within 2 to 5 milliseconds. My guess is this
+is not unusual for these cheapo tubes. A reminder that this is a toy,
+not a real instrument.
+
+To work around this I modified the code to ignore pulses less than 10 ms
+after the preceding one. This seems to provide reasonable counts again.
+*/
+
 
 #include <util/atomic.h>
 #define ALARM_CPM 70
+#define MIN_INTERVAL 10  
 const int geiger_pin = 2;
 const int meter_pin = 3;
 const int count_led_pin = 13;
 const int alarm_led_pin = 9;
 const int n=20;
 volatile int pos=0;
-volatile long buf[n];
+
+volatile long buf[n], now, prevbuf;
 volatile int led=0;
 volatile bool triggered=false;
 const int maxcpm=100;
@@ -27,14 +41,18 @@ void setup() {
   digitalWrite(alarm_led_pin, HIGH);
   // Ring buffer
   for (int i=0; i<n; i++) { buf[i] = -1; }
+  prevbuf = -9999;
 
   Serial.begin(115200);
   pinMode(geiger_pin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(geiger_pin), isrcount, FALLING);
+  // LOW may be more resistant to noise than FALLING
+  // I need a better oscilloscope to know for sure
+  attachInterrupt(digitalPinToInterrupt(geiger_pin), isrcount, LOW);
 }
 
 void loop() {
     long avg, cpm;
+
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {   
         if (buf[pos] != -1 && triggered) {
             // ms between pulses
@@ -44,13 +62,14 @@ void loop() {
             triggered = false;
         }
     }
+
     if (cpm > ALARM_CPM) { digitalWrite(alarm_led_pin, HIGH); } 
     else { digitalWrite(alarm_led_pin, LOW); }
     if ((millis()- printtimer)  > 10000) {
-//            printbuffer();
+            printbuffer();
             if (buf[pos] != -1) { 
-//                Serial.print(avg);
-//                Serial.print("\t");
+                Serial.print(avg);
+                Serial.print("\t");
                 Serial.print(cpm);
             }
             Serial.println();
@@ -59,12 +78,20 @@ void loop() {
 }
 
 void isrcount() {
-  buf[pos] = millis();
+  now = millis();
+  // hold down quench failure. Don't touch the ring buffer. Discard "now".
+  if ((now - prevbuf) < MIN_INTERVAL) { 
+        prevbuf = now;
+        return;
+  }
+  buf[pos] = now;
+  prevbuf = buf[pos];
   pos++;
   if (pos > n-1) { pos = 0; }
   led = !led;
   digitalWrite(count_led_pin, led);
   triggered = true;
+  return;
 }
 
 void printbuffer() { 
@@ -80,6 +107,5 @@ void printbuffer() {
            Serial.println();
         }
     }
-    //delay(1000);
 }
 
